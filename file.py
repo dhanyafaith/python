4d4864d99954baba2d577278619e24d08896e83a
dhanya <alexdhanya@gmail.com>
2023-02-15 11:56:53 +0530

    first commit

---
+++ file.py
@@ -0,0 +1,20 @@
+name="dhanya"
+age= 23
+print(name)
+print ("I am " + name)
+print ("Age is :", age)
+list = [1,2,3,"ball","apple",True]
+print(list)
+print(type(list))
+tuple=(1,2,3,"hello")
+print(tuple)
+print(type(tuple))
+dictionary = {1:'apple',2:'ball',"name":"Anu"}
+print(dictionary)
+print(type(dictionary))
+set= {1,2,3,4,1,2,3}
+print(set)
+print(type(set))
+set1={frozenset(set)}
+print (set1)
+print(type(set1))
